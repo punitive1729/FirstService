894eea505cddf5c081b99eefc70441e0937695a6
Punit <ojha1111pk@gmail.com>
2023-04-12 13:50:50 +0530

    updated second service url

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const app = express();
 const PORT = 8000;
 const TIME_INTERVAL = 700000;
 const TIC_TAC_TOE_URL = 'https://tic-tac-toe-c5q2.onrender.com/health';
-const SECOND_SERVICE = '';
+const SECOND_SERVICE_URL = 'https://second-service-56j6.onrender.com/health';
 
 app.listen(PORT, () => {
   console.log(`App is running on Port: ${PORT}`);
   setInterval(async () => {
-    const response = await axios.get(TIC_TAC_TOE_URL);
-    console.log('Response : ', response.data);
+    try {
+      let response = await axios.get(TIC_TAC_TOE_URL);
+      console.log('Response from TIC-TAC-TOE : ', response.data);
+      response = await axios.get(SECOND_SERVICE_URL);
+      console.log('Response from SECOND_SERVICE: ', response.data);
+    } catch (err) {
+      console.log('Error: ', err);
+    }
   }, TIME_INTERVAL);
 });
 
