f0df3be25644a6bd7a5d806c094a933d28d46bc4
Punit <ojha1111pk@gmail.com>
2023-04-15 18:11:32 +0530

    changed message

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ const TIME_INTERVAL = 700000;
 const TIC_TAC_TOE_URL = 'https://tic-tac-toe-c5q2.onrender.com/health';
 const SECOND_SERVICE_URL = 'https://second-service-56j6.onrender.com/health';
 
+const connect = async (URL) => {
+  try {
+    await axios.get(URL);
+    console.log(`positive response from: ${URL}`);
+  } catch (err) {
+    console.log(`negative response from: ${URL}`);
+  }
+};
+
 app.listen(PORT, () => {
   console.log(`App is running on Port: ${PORT}`);
   setInterval(async () => {
-    try {
-      let response = await axios.get(TIC_TAC_TOE_URL);
-      console.log('Response from TIC-TAC-TOE : ', response.data);
-      response = await axios.get(SECOND_SERVICE_URL);
-      console.log('Response from SECOND_SERVICE: ', response.data);
-    } catch (err) {
-      console.log('Error: ', err);
-    }
+    await connect(TIC_TAC_TOE_URL);
+    await connect(SECOND_SERVICE_URL);
   }, TIME_INTERVAL);
 });
 
